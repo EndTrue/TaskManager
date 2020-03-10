f1899b1b9650d108ab6282a5ce67ca666f831062
DarkSide <blacklife1992@gmail.com>
2020-03-10 18:00:08 +0200

    Intermediate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   root to: 'landing#index'
+
+  namespace :api do
+    resources :projects
+    resources :tasks
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
